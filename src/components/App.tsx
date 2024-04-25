7a664c16cab3957303f17699e59df1187b927954
Mikhail Gilev <gilev820@gmail.com>
2024-04-25 15:07:08 +0500

    fix: missing texture error

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,6 +2,13 @@ import { ChakraProvider } from '@chakra-ui/react';
 import { theme } from '@peach/config';
 import { AppProvider, ElementsProvider, ThemeProvider } from '@peach/contexts';
 import { Main } from '@peach/components';
+import { Spine } from '@pixi-spine/runtime-4.1';
+import { SpineMesh } from '@pixi-spine/base';
+import { Texture } from 'pixi.js';
+
+Spine.prototype.newMesh = function (texture, vertices, uvs, indices, drawMode) {
+  return new SpineMesh(texture ?? Texture.EMPTY, vertices, uvs, indices, drawMode);
+};
 
 export default function App() {
   return (
